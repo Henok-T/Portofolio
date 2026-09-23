d571048be94253bfaf8952de1f3cc32bf892f43e
Henok T. <hk.tsegai@yahoo.com>
2026-09-23 07:24:49 -0400

    removed files

--- src/lib/multiplayer/index.ts
+++
@@ -1,9 +0,0 @@
-export { P2PRoom, defaultIceServers } from "./p2p";
-export type {
-  PeerInfo,
-  P2PRoomOptions,
-  SignalKind,
-  PeerRow,
-  SignalRow,
-  RtcPollResponse,
-} from "./p2p";
--- src/routes/contact.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/contact")({
-  beforeLoad: () => {
-    throw redirect({ to: "/", hash: "contact" });
-  },
-});
